ec07a50090c002ed14081bfa9d7ca8eaadb88da0
Yesui <yesui1209@gmail.com>
2021-05-07 18:48:53 +0800

    finance module ruu add hiideg bolgono

--- app.js
+++ app.js
@@ -25,21 +25,23 @@ var uiController = (function() {
 
 // Санхүүтэй холбоотой контроллер
 var financeController = (function() {
-
+    // private data
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
+    // private data
     var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
+    // private data
     var data = {
-        allItems: {
+        items: {
             inc: [],
             exp: []
         },
@@ -48,16 +50,40 @@ var financeController = (function() {
             inc: 0,
             exp: 0
         }
-    }
+    };
 
+    return {
+        addItem: function(type, desc, val) {
+            var item, id;
+
+            if(data.items[type].length === 0) id = 1;
+            else {
+                id = data.items[type][data.items[type].length - 1].id + 1;
+            }
+
+            if(type === 'inc') {
+                item = new Income(id, desc, val);
+            } else {
+                item = new Expense(id, desc,val);
+            }
+
+            data.items[type].push(item);
+        },
+
+        seeData: function() {
+            return data;
+        }
+    }
 })();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
     var ctrlAddItem = function () {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-        console.log(uiController.getInput());
+        var input = uiController.getInput();
+
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        financeController.addItem(input.type, input.description, input.value);
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
